1c23181c5c1659dd625e25356a6dff418740c344
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-07-19 17:56:29 +0530

    feat(budgets): register budgets view model

--- shared/ui/src/commonMain/kotlin/com/tioledger/ui/di/TioUiModules.kt
+++ shared/ui/src/commonMain/kotlin/com/tioledger/ui/di/TioUiModules.kt
@@ -1,6 +1,7 @@
 package com.tioledger.ui.di
 
 import com.tioledger.ui.accounts.AccountsViewModel
+import com.tioledger.ui.budgets.BudgetsViewModel
 import com.tioledger.ui.categories.CategoriesViewModel
 import com.tioledger.ui.transactions.TransactionEntryViewModel
 import com.tioledger.ui.transactions.TransactionsViewModel
@@ -10,6 +11,7 @@ import org.koin.dsl.module
 fun tioUiModule(): Module =
     module {
         factory { AccountsViewModel(get()) }
+        factory { BudgetsViewModel(get(), get(), get(), get(), get()) }
         factory { CategoriesViewModel(get(), get(), get()) }
         factory { TransactionEntryViewModel(get(), get(), get(), get(), get()) }
         factory { TransactionsViewModel(get()) }
