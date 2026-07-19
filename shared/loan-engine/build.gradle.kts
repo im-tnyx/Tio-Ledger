73abf4ea10f1faa9627408c082e02a445dac4c63
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-07-19 22:03:29 +0530

    build(loans): add datetime dependency to loan engine

--- shared/loan-engine/build.gradle.kts
+++ shared/loan-engine/build.gradle.kts
@@ -7,6 +7,7 @@ kotlin {
         commonMain.dependencies {
             api(project(":shared:core"))
             api(project(":shared:finance-engine"))
+            implementation(libs.kotlinx.datetime)
         }
 
         commonTest.dependencies {
